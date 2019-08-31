140ffda9bcbc3776dd2b059b73f73c65593381ab
Kevin Castell <castellkevin10@gmail.com>
2019-08-30 20:46:53 -0600

    Timing file changes

--- venv/lib/Timing_MrAnaga.py
+++ venv/lib/Timing_MrAnaga.py
@@ -5,7 +5,8 @@ import string
 
 
 def main():
-    calculating_runtime_doubling_n()
+    #calculating_runtime_doubling_n()
+    calculating_runtime_doubling_k()
 
 
 def random_string_genrator(length):
@@ -23,13 +24,32 @@ def calculating_runtime_doubling_n():
         run_time, count = running_test(test_input)
         final_time = run_time / count
         print(str(n) + '\t' + str(final_time))
+        n = n * 2
 
-    return run_times
+
+def calculating_runtime_doubling_k():
+    k = 4
+    n = 2000
+    while k < 2**16:
+        test_input = []
+        for i in range(n):
+            test_input.append(random_string_genrator(k))
+
+        run_time, count = running_test(test_input)
+        final_time = run_time / count
+        print(str(k) + '\t' + str(final_time))
+        k = k*2
 
 
 def running_test(test_input):
     t = 0
     count = 0
+    t0 = time()
+    while t <= 0.20:
+        MrAnagram.main(test_input)
+        count = count + 1
+        t = time() - t0
+
     t0 = time()
     while t <= 5:
         MrAnagram.main(test_input)
